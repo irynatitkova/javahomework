084a5bc125a104b3fc1eed91637779db3dc11db1
irynatitkova <125497951+irynatitkova@users.noreply.github.com>
2023-02-25 12:56:22 -0600

    HomeworkJava22.02.2023
    Задание 1
    установить JDK 11, 17
    создать два проекта Java, переключить в настройках один на 11, второй на 17
    Задание 2
    В любом из проектов создать несколько разных пакетов
    В каждом пакете создать класс(Java-файл), который при запуске программы выводит сообщения "Привет. Я -  подставить название класса"

---
+++ Homework22.02/Project2/src/Javafile.java
@@ -0,0 +1,10 @@
+import basicjava.tel.ran.Hello;
+
+public class Javafile {
+public static void main(String[] args) {
+    Hello welcome = new Hello();
+    System.out.println ("Привет. Я - "+welcome.welcome);
+}
+
+
+}
