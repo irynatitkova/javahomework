b2ac8432c7d9d3b6ff49a66575ca9a53ac5cacb9
irynatitkova <125497951+irynatitkova@users.noreply.github.com>
2023-03-04 11:33:46 -0600

    JavaHomework03.03.
     1 уровень сложности: Задание 1:
    Создать переменные типа boolean, int, String, double.
    Каждой переменной задать значение в соответствии с этим типом.
    
    Вывести значение каждой переменной на экран. 
    
    
    Задание 2:
    
    
    
    Написать 4 метода в любом классе каждый из которых соответствует определённо математической операции(+, -, /, *) - сложение, вычитание, деление, умножение. 
    
    В каждом методе две целочисленные переменные должны быть созданы с определённым значением, с ними должна быть проведена математическая операция, соответствующая  названию метода и выведен результат.
    Эти методы должны быть вызваны из метода "main".
    2 уровень сложности:

---
+++ Homework0303/printer0303/src/Main.java
@@ -0,0 +1,47 @@
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        add();
+        subtract();
+        divide();
+        multiply();
+    }
+
+    public static void add() {
+        int number1 = 0;
+        int number2 = 10;
+        int result = number1 + number2;
+        System.out.println(result);
+    }
+
+    public static void divide() {
+        int number3 = 36;
+        int number4 = 12;
+        int result = number3 / number4;
+        System.out.println(result);
+    }
+
+    public static void multiply() {
+        int number7 = 10;
+        int number8 = 100;
+        int result = number7 * number8;
+        System.out.println(result);
+    }
+
+    public static void subtract() {
+        int number10 = 1100;
+        int number11 = 11;
+        int result = number10 - number11;
+        System.out.println(result);
+    }
+    private static void addition2() {
+        int number20 = 10;
+        int number21 = 11;
+        int result = number20 + number21;
+        System.out.println(result);
+    }
+
+}
+
---
+++ Homework0303/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        String str =" amount of cars";
+        String str2 ="202112212";
+        int number = 9;
+        int number22 = 10;
+        boolean b1 = true;
+        boolean b2 = false;
+        double Number1  = 5;
+        double Number12 = 13;
+
+        System.out.println(Number1 / Number12);
+        System.out.println(number);
+        System.out.println(number22);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println("Hello world!");
+    }
+}
